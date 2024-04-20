85a62d65bee4dd545f6d4f1aa7da2fc2d4d5e592
Andre <andrevitorlopes@gmail.com>
2024-04-20 20:53:21 +0200

    Fix condition values type

--- iac/website.tf
+++ iac/website.tf
@@ -45,7 +45,7 @@ data "aws_iam_policy_document" "bucket_policy" {
     condition {
       test     = "StringEquals"
       variable = "AWS:SourceArn"
-      values   = aws_cloudfront_distribution.s3_distribution.arn
+      values   = [aws_cloudfront_distribution.s3_distribution.arn]
     }
   }
 }
