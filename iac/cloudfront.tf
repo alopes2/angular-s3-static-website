342fe8fc8c08ed8eb3923ecd40ba298e0cde3acd
Andre <andrevitorlopes@gmail.com>
2024-04-20 20:26:25 +0200

    Add cache policy

--- iac/cloudfront.tf
+++ iac/cloudfront.tf
@@ -33,13 +33,12 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = local.website_origin_id
 
-    origin_request_policy_id = aws_cloudfront_origin_request_policy.website_origin_request_policy.id
+    cache_policy_id = aws_cloudfront_cache_policy.website.id
 
     viewer_protocol_policy = "allow-all"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
-
   }
 
   price_class = "PriceClass_100"
@@ -58,17 +57,35 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
 }
 
-resource "aws_cloudfront_origin_request_policy" "website_origin_request_policy" {
-  name = "origin_request_policy"
-  headers_config {
-    header_behavior = "allViewer"
-  }
+# Sends to the origin and caches it
+resource "aws_cloudfront_cache_policy" "website" {
+  name = "angular_cache_policy"
 
-  cookies_config {
-    cookie_behavior = "all"
-  }
-
-  query_strings_config {
-    query_string_behavior = "all"
-  }
+  parameters_in_cache_key_and_forwarded_to_origin {
+    headers_config {
+      header_behavior = "allViewer"
+    }
+    cookies_config {
+      cookie_behavior = "all"
+    }
+    query_strings_config {
+      query_string_behavior = "all"
+    }
 }
+
+# Sends to the origin but doesn't cache it
+# Must be set with a Cache Policy
+# resource "aws_cloudfront_origin_request_policy" "website_origin_request_policy" {
+#   name = "origin_request_policy"
+#   headers_config {
+#     header_behavior = "allViewer"
+#   }
+
+#   cookies_config {
+#     cookie_behavior = "all"
+#   }
+
+#   query_strings_config {
+#     query_string_behavior = "all"
+#   }
+# }
