c1efd37b4134aed8df73771d0626a4dec5b2a3f4
Andre Lopes <andrevitorlopes@gmail.com>
2024-04-20 16:48:37 +0200

    Test routes

--- src/src/app/app.routes.ts
+++ src/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TestComponent } from './test/test.component';
+import { CarComponent } from './car/car.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'test',
+    component: TestComponent,
+  },
+  {
+    path: 'car',
+    component: CarComponent,
+  },
+  { path: '**', redirectTo: '/' },
+];
