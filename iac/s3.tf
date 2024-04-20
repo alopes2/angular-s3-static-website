fb691e70ee052992571f1ffc95171de55b3bb7b9
Andre <andrevitorlopes@gmail.com>
2024-04-20 14:33:17 +0200

    Start article application

---
+++ iac/s3.tf
@@ -0,0 +1,47 @@
+resource "aws_s3_bucket" "website" {
+  bucket = "angular-s3-static-website"
+}
+
+resource "aws_s3_bucket_policy" "public_bucket_policy" {
+  bucket = aws_s3_bucket.website.id
+  policy = data.aws_iam_policy_document.bucket_policy.json
+}
+
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+
+  routing_rule {
+    condition {
+      key_prefix_equals = "docs/"
+    }
+    redirect {
+      replace_key_prefix_with = "index.html"
+    }
+  }
+
+}
+
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "arn:aws:s3:::angular-s3-static-website/*"
+    ]
+  }
+}
